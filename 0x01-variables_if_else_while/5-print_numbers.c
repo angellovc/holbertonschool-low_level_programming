6559ae185aaa0e03bd3ebaf898671e242608294f
angellovc <vc.angel@outlook.com>
2020-02-06 19:20:54 +0000

    new file was added

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char number;
+
+	number = 48;
+	while (number <= 57)
+	{
+		putchar(number);
+		number++;
+	}
+	putchar('\n');
+	return (0);
+}
