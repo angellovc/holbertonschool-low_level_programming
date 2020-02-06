1d0c74fcfcd60ad7cac0f69ec5d408d785f7cadd
angellovc <vc.angel@outlook.com>
2020-02-06 20:01:00 +0000

    new file was added

--- 0x01-variables_if_else_while/6-print_numberz.c
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -6,24 +6,13 @@
  */
 int main(void)
 {
-	int number[10] = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9};
-	int i = 0;
-	int character[10];
+	int number;
 
-	character[0] = number[0] + '0';
-	character[1] = number[1] + '0';
-	character[2] = number[2] + '0';
-	character[3] = number[3] + '0';
-	character[4] = number[4] + '0';
-	character[5] = number[5] + '0';
-	character[6] = number[6] + '0';
-	character[7] = number[7] + '0';
-	character[8] = number[8] + '0';
-	character[9] = number[9] + '0';
-	while (i <= 10)
+	number = 0;
+	while (number < 9)
 	{
-		putchar(character[i]);
-		i++;
+		putchar('0' + number);
+		number++;
 	}
 	putchar('\n');
 	return (0);
