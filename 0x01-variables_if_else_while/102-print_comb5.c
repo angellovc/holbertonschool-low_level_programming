f9fb38ec51386139b1c0c025ee478dcc43030156
angellovc <vc.angel@outlook.com>
2020-02-07 23:40:48 +0000

    new file was added

---
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int hour1, hour2, min, seg, comma = 44, space = 32;
+
+	for (hour1 = 48; hour1 <= 57; hour1++)
+	{
+		for (hour2 = 48; hour2 <= 57; hour2++)
+		{
+			for (min = 48; min <= 57; min++)
+			{
+				for (seg = 48; seg <= 57; seg++)
+				{
+					putchar(hour1);
+					putchar(hour2);
+					putchar(space);
+					putchar(min);
+					putchar(seg);
+					if (!(hour1 == 57 && hour2 == 57 && min == 57 && seg == 57))
+					{
+						putchar(comma);
+						putchar(space);
+					}
+				}
+			}
+		}
+	}
+	putchar('\n');
+	return (0);
+}
