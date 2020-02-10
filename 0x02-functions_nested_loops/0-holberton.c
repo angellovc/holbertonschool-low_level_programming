27aa7b708779136ef5ebe08ed41f293ee159259a
angellovc <vc.angel@outlook.com>
2020-02-10 22:27:27 +0000

    new file was added

--- 0x02-functions_nested_loops/0-holberton.c
+++ 0x02-functions_nested_loops/0-holberton.c
@@ -1,6 +1,20 @@
-#include <holberton.h>
-
+#include "holberton.h"
+/**
+ *main - entry point
+ *
+ *Return: always is 0
+ */
 int main(void)
 {
+	int i;
+	char word[9] = {'H', 'o', 'l', 'b', 'e', 'r', 't', 'o', 'n'};
 
+	i = 0;
+	while (i < 9)
+	{
+		_putchar(word[i]);
+		i++;
+	}
+	putchar('\n');
+	return (0);
 }
