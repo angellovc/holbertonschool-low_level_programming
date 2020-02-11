4942ff52771284ad568821fcf46a625d7d85deb6
angellovc <vc.angel@outlook.com>
2020-02-11 19:58:21 +0000

    new file was added

---
+++ 0x03-debugging/holberton.h
@@ -0,0 +1,10 @@
+carrie@ubuntu:/debugging$ cat holberton.h
+#ifndef HOLBERTON_H
+#define HOLBERTON_H
+
+#include <stdio.h>
+
+void positive_or_negative(int i);
+
+#endif /* HOLBERTON_H */
+carrie@ubuntu:/debugging$ 
