0e253f4482e613ddc80f3b8f6cac25d12e05db45
angellovc <vc.angel@outlook.com>
2020-02-11 20:51:40 +0000

    new file was added

--- 0x03-debugging/holberton.h
+++ 0x03-debugging/holberton.h
@@ -1,10 +1,9 @@
-carrie@ubuntu:/debugging$ cat holberton.h
 #ifndef HOLBERTON_H
 #define HOLBERTON_H
 
 #include <stdio.h>
 
 void positive_or_negative(int i);
+int largest_number(int a, int b, int c);
 
 #endif /* HOLBERTON_H */
-carrie@ubuntu:/debugging$ 
