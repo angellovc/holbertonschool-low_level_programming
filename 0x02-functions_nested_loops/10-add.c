20f9add00ecee0b74c9ac759b7897e61218e0104
angellovc <vc.angel@outlook.com>
2020-02-11 17:22:13 +0000

    new file was added

--- 0x02-functions_nested_loops/10-add.c
+++ 0x02-functions_nested_loops/10-add.c
@@ -1,9 +1,15 @@
 #include "holberton.h"
-
+/**
+ *add - add two numbers
+ *@a: receive an int
+ *@b: receive an int
+ *
+ *Return: return the sum
+ */
 int add(int a, int b)
 {
 	int ressult = 0;
 
 	ressult = a + b;
-	return(ressult);
+	return (ressult);
 }
