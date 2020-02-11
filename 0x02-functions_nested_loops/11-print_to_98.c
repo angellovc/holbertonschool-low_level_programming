98b1b16b5775b682c5ccdacdd58aaf88f9e0bd5d
angellovc <vc.angel@outlook.com>
2020-02-11 18:37:07 +0000

    new file was added

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -14,11 +14,13 @@ void print_to_98(int n)
 	{
 		for (i = n; i >= 98; i--)
 		{
-			printf("%i, ", i);
 			if (i == 98)
 			{
 				printf("%i\n", i);
 			}
+			else
+				printf("%i, ", i);
+
 		}
 	}
 	else if (n == 98)
@@ -27,12 +29,12 @@ void print_to_98(int n)
 	{
 		for (i = n; i <= 98; i++)
 		{
-			printf("%i, ", i);
 			if (i == 98)
 			{
 				printf("%i\n", i);
 			}
-
+			else
+				printf("%i, ", i);
 		}
 	}
 }
