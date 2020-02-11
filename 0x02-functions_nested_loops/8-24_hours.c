bcaa5a6663ba2f2691c670ab57eafbc95916f5ef
angellovc <vc.angel@outlook.com>
2020-02-11 15:30:27 +0000

    new file was added

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,30 @@
+#include "holberton.h"
+/**
+ * jack_bauer - clock function
+ *
+ *Return: don't return anything
+ */
+void jack_bauer(void)
+{
+	int h1, h2, m1, m2, space = 32;
+
+	for (h1 = 48; h1 <= 50; h1++)
+	{
+		for (h2 = 48; h2 <= 51; h2++)
+		{
+			for (m1 = 48; m1 <= 53; m1++)
+			{
+				for (m2 = 48; m2 <= 57; m2++)
+				{
+					_putchar(h1);
+					_putchar(h2);
+					_putchar(space);
+					_putchar(m1);
+					_putchar(m2);
+					_putchar(space);
+					_putchar('\n');
+				}
+			}
+		}
+	}
+}
