c1fd7116a87898579a9035234ea048d87babc9be
angellovc <vc.angel@outlook.com>
2020-02-13 21:25:56 +0000

    new file was added

---
+++ 0x04-more_functions_nested_loops/100-prime_factor.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+ *main - entry point
+ *
+ *Return: 0
+ */
+int main(void)
+{
+	long int y = 2, number = 612852475143;
+	while (number != 1)
+	{
+		if (number % y == 0)
+			number = number / y;
+		else
+			y++;
+	}
+	printf("%li\n", y);
+	return (0);
+}
