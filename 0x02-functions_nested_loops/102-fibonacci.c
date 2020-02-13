e0e4cc6a8d9c5301ee2776a455c8596c54a1d304
angellovc <vc.angel@outlook.com>
2020-02-13 01:09:58 +0000

    new file was added

---
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+/**
+ *main - entry point
+ *
+ *Return: nothing
+ */
+int main(void)
+{
+	long int number1 = 1, number2 = 2, temp;
+	int i;
+
+	printf("%li, %li, %li, ", number1, number1, number2);
+	for (i = 1; i <= 50; i++)
+	{
+		temp = number1 + number2;
+		if (i >= 50)
+			printf("%li\n", temp);
+		else
+			printf("%li, ", temp);
+		number1 = number2;
+		number2 = temp;
+	}
+}
