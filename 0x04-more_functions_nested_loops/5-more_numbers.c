f766ed9da16c9a765bd100364b5b94a22889f235
angellovc <vc.angel@outlook.com>
2020-02-13 17:36:11 +0000

    new file was added

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -11,13 +11,13 @@ void more_numbers(void)
 	while (i <= 10)
 	{
 		for (n = 0; n <= 14; ++n)
-			{
-				last_digit = n % 10;
-				next_digit = n / 10;
-				if (n > 9)
-					_putchar('0' + next_digit);
-				_putchar('0' + last_digit);
-			}
+		{
+			last_digit = n % 10;
+			next_digit = n / 10;
+			if (n > 9)
+				_putchar('0' + next_digit);
+			_putchar('0' + last_digit);
+		}
 		i++;
 		_putchar('\n');
 	}
