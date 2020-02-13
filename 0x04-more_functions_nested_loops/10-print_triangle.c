eda349278f2291e92c7450d24f7c9e86ba020ae9
angellovc <vc.angel@outlook.com>
2020-02-13 19:45:06 +0000

    new file was added

--- 0x04-more_functions_nested_loops/10-print_triangle.c
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -1,5 +1,9 @@
 #include "holberton.h"
-
+/**
+ *print_triangle - size
+ *@size: receive a number
+ *
+ */
 void print_triangle(int size)
 {
 	int i, j, y;
@@ -8,7 +12,7 @@ void print_triangle(int size)
 		_putchar('\n');
 	for (i = 1; i <= size; i++)
 	{
-		for (j = i; j <= size; j++)
+		for (j = i; j < size; j++)
 			_putchar(' ');
 		for (y = 1; y <= i; y++)
 			_putchar('#');
