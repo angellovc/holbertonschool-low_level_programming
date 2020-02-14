a639161ce9ee2abef35d9e9b62df0fbe7670ccbf
angellovc <vc.angel@outlook.com>
2020-02-14 21:07:59 +0000

    new file was added

--- 0x02-functions_nested_loops/103-fibonacci.c
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -6,16 +6,19 @@
  */
 int main(void)
 {
-	long int number1 = 1, number2 = 2, temp = 0;
+	long int number1 = 1, number2 = 2, temp = 0, even = 2;
 
 	printf("%li, %li", number1, number2);
-	while (temp < 4000000)
+	while (temp <= 4000000)
 	{
 		temp = number1 + number2;
-		if (temp % 2 == 0)
-			printf(", %li", temp);
 		number1 = number2;
 		number2 = temp;
+		if (temp % 2 == 0)
+		{
+			even = even + temp;
+			printf(", %li", even);
+		}
 	}
 	printf("\n");
 	return (0);
