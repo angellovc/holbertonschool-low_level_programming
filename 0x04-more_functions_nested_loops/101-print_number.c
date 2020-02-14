c29f4d222378b60e710c71907793b4372adac8af
angellovc <vc.angel@outlook.com>
2020-02-14 00:21:16 +0000

    new file was added

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -11,7 +11,7 @@ void print_number(int n)
 	else if (n == 0)
 		_putchar(n + '0');
 	else
-		reverse_negative(n);
+		reverse(n);
 }
 /**
  *reverse - reverse numbers
@@ -22,36 +22,17 @@ void reverse(int n)
 {
 	int reverse_n = 0, last_number;
 
+	if (n < 0)
+	{
+		n = -n;
+		_putchar(45);
+	}
 	while (n != 0)
 	{
 		last_number = n % 10;
 		reverse_n = (reverse_n * 10) + last_number;
 		n = n / 10;
 	}
-	while (reverse_n != 0)
-	{
-		last_number = reverse_n % 10;
-		reverse_n = reverse_n / 10;
-		_putchar('0' + last_number);
-	}
-}
-/**
- *reverse_negative - reverse negative numbers
- *@n: receive a number
- *
- */
-void reverse_negative(int n)
-{
-	int reverse_n = 0, last_number, i;
-
-	i = -n;
-	while (i != 0)
-	{
-		last_number = i % 10;
-		reverse_n = (reverse_n * 10) + last_number;
-		i = i / 10;
-	}
-	_putchar(45);
 	while (reverse_n != 0)
 	{
 		last_number = reverse_n % 10;
