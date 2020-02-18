3896296d148636eee95c78ad0f59e41f44a7a413
angellovc <vc.angel@outlook.com>
2020-02-18 21:02:46 +0000

    new file was added

---
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -0,0 +1,26 @@
+#include "holberton.h"
+/**
+ *_atoi - this is and atoi function
+ *@s: receive an array
+ *
+ *Return: the numbers in array
+ */
+int _atoi(char *s)
+{
+	int i, n1 = 0, number = 0;
+	int  sign = 1;
+
+	for (i = n1; s[i] != '\0' ; i++)
+	{
+		if (s[i] >= '0' && s[i] <= '9')
+			number = (number * 10) + s[i] - '0';
+		else if (s[i] == '-')
+			sign = sign * -1;
+		else if (s[i] == '+')
+			sign = sign * 1;
+
+		if (s[i] < 57 && s[i] > 48 && s[i + 1] < 48 && s[i + 1] < 57)
+			break;
+	}
+	return (number * sign);
+}
