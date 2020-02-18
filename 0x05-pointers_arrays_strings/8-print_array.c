352961b67fab555fd23dda04396c636777e3a71b
angellovc <vc.angel@outlook.com>
2020-02-18 17:39:24 +0000

    new file was added

--- 0x05-pointers_arrays_strings/8-print_array.c
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -11,8 +11,9 @@ void print_array(int *a, int n)
 	for (i = 0; i < n; i++)
 	{
 		if (i == (n - 1))
-			printf("%i\n", a[i]);
+			printf("%i", a[i]);
 		else
 			printf("%i, ", a[i]);
 	}
+	printf("\n");
 }
