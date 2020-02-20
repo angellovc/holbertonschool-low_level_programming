0991c90f598a6403a5a94ee34cdb6ee87d8010ef
angellovc <vc.angel@outlook.com>
2020-02-20 23:45:08 +0000

    new file was added

---
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -0,0 +1,20 @@
+#include "holberton.h"
+
+char *leet(char *s)
+{
+	int i = 0, y = 0;
+	char alpha[] = "AaEeOoTtLl";
+	char number[] = "4433007711";
+
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		while (alpha[y] != '\0')
+		{
+			if (s[i] == alpha[y])
+				s[i] = number[y];
+			y++;
+		}
+		y = 0;
+	}
+	return (s);
+}
