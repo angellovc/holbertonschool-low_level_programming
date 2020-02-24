ebb8f03379f064a01bb689fcd1e8e8dd90a9fc59
angellovc <vc.angel@outlook.com>
2020-02-24 18:22:46 +0000

    new file was added

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -8,16 +8,14 @@
 char *_strchr(char *s, char c)
 {
 	unsigned int i = 0;
-	char *p = '\0';
 
-	while (s[i])
+	while (s[i] != '\0')
 	{
 		if (s[i] == c)
-		{
-			p = *(s + i);
-			return (p);
-		}
+			return (&s[i]);
 		i++;
 	}
-	return (p);
+		if (s[i] == c)
+			return (&s[i]);
+	return (&s[i]);
 }
