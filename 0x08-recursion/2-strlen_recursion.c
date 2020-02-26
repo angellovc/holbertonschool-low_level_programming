6191a6fcb6c32a4927f209c017b8103e810cc547
angellovc <vc.angel@outlook.com>
2020-02-26 17:40:50 +0000

    new file was added

---
+++ 0x08-recursion/2-strlen_recursion.c
@@ -0,0 +1,16 @@
+#include "holberton.h"
+#include <stdio.h>
+/**
+ *_strlen_recursion - find the array lenght using recursion
+ *@s: array addres
+ *Return: number of character
+ */
+int _strlen_recursion(char *s)
+{
+	int n = 0;
+
+	if (*(s + 1) == '\0')
+		return (n);
+	n = 1 +	_strlen_recursion(s + 1);
+	return (n);
+}
