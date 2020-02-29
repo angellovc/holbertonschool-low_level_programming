7b8179f984fe90d0f98c8407549a3945c4181cd3
angellovc <vc.angel@outlook.com>
2020-02-29 20:22:51 +0000

    new file was added

---
+++ 0x0A-argc_argv/.#1-args.c
@@ -0,0 +1,1 @@
+root@vagrant-ubuntu-trusty-64.1604:1583005564
--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -1,37 +1,79 @@
 #include "holberton.h"
 #include <stdio.h>
 #include <stdlib.h>
-
+/**
+ *main - entry point
+ *@argc: number of arguments
+ *@argv: arguments
+ *Return: 0
+ */
 int main(int argc, char **argv)
 {
-	int y;
+	int y = 0, ressult = 0, i = 0;
 
+	if (argc == 1)
+	{
+	printf("0\n");
+		return (0);
+	}
 	y = letters_digits(argc, argv);
-	printf("%i\n", y);
+	if (y == 2)
+	{
+	printf("0\n");
+		return (0);
+	}
+	if (y == 3)
+	{
+	printf("Error\n");
+		return (0);
+	}
+	for (i = 1; i < argc; i++)
+		ressult = ressult + atoi(argv[i]);
+	printf("%i\n", ressult);
 	return (0);
 }
-
+/**
+ *letters_digits - evalue if you got characters, numbers or mixed
+ *@argc: number of arguments
+ *@s: arguments
+ *Return: 3 mixed, 2 characters, 1 just numbers
+ */
 int letters_digits(char argc, char **s)
 {
-	int i, y, flag = 0;
-	for (i = 0; i < argc; i++)
-		for (y = 0; (s[i][y] =! '\0'); y++)
+	int i = 0, y = 0, flag = 0;
+
+	for (i = 1; i < argc; i++)
+	{
+		for (y = 0; (s[i][y] != '\0'); y++)
 		{
 			if (s[i][y] < 48 || s[i][y] > 57)
+			{
+				flag = 0;
 				break;
+			}
 			else if (s[i][y + 1] == '\0')
 				flag = 1;
 		}
+		if (flag != 1)
+			break;
+	}
 	if (flag == 1)
 		return (1);
-	for (i = 0; i < argc; i++)
-		for (y = 0; s[i][y] != '\0'; i++)
+	for (i = 1; i < argc; i++)
+	{
+		for (y = 0; s[i][y] != '\0'; y++)
 		{
-			if (s[i][y + 1] >= 48 && s[i][y] <= 57)
+			if (s[i][y] >= 48 && s[i][y] <= 57)
+			{
+				flag = 0;
 				break;
+			}
 			else if (s[i][y + 1] == '\0')
 				flag = 2;
 		}
+		if (flag != 2)
+			break;
+	}
 	if (flag == 2)
 		return (2);
 	return (3);
