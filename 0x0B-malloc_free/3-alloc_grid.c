1f9e73a0d053e84abf96a85c6189b6bc0a01f64b
angellovc <vc.angel@outlook.com>
2020-03-03 20:46:28 +0000

    new file was added

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -12,7 +12,7 @@ int **alloc_grid(int width, int height)
 	if (width < 1 || height < 1)
 		return ('\0');
 
-	grid = (int **)malloc(height * sizeof(int *));
+	grid = malloc(height * sizeof(int **));
 	if (grid == '\0')
 		return ('\0');
 
