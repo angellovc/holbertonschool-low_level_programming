e55bfdadec1e546ee33419a7ca9f8baf2b46ac69
angellovc <vc.angel@outlook.com>
2020-03-03 17:30:47 +0000

    new file was added

---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,23 @@
+#include <stdlib.h>
+/**
+ *_strdup - copy a string into other
+ *@str: string
+ *Return: pointer to string
+ */
+char *_strdup(char *str)
+{
+	unsigned int i = 0;
+	char *copy;
+
+	if (*str == '\0')
+		return ('\0');
+	copy = malloc(sizeof(str));
+	if (copy == '\0')
+		return ('\0');
+	while (i <= (sizeof(str) / sizeof(char)))
+	{
+		copy[i] = str[i];
+		i++;
+	}
+	return (copy);
+}
