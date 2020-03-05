0cd31bc3183865644e75aa6da77e96e6f77137a5
angellovc <vc.angel@outlook.com>
2020-03-05 17:53:50 +0000

    new file was added

---
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -0,0 +1,46 @@
+#include <stdlib.h>
+#include <stdio.h>
+/**
+ *_strlen - mesure the lenght of an array
+ *@s: receive an array
+ *
+ *Return: i
+ */
+int _strlen(char *s)
+{
+	unsigned int i = 0;
+
+	while (*(s + i) != '\0')
+		i++;
+	return (i);
+}
+/**
+ *string_nconcat - concatenate s1 and s2 n times
+ *@n: number
+ *@s1: string
+ *@s2: string
+ *Return: pointer to a new string
+ */
+char *string_nconcat(char *s1, char *s2, unsigned int n)
+{
+	unsigned int l1 = 0, l2 = 0, y = 0, i = 0;
+	char *p;
+
+	l1 = _strlen(s1);
+	l2 = _strlen(s2);
+	if (n >= l2)
+	{
+		p = malloc(l1 + l2 + 1);
+		n = l2;
+	}
+	else
+		p = malloc(l1 + n + 1);
+	for (i = 0; s1[i] != '\0'; i++)
+		p[i] = s1[i];
+	for (y = 0; y < n; y++)
+	{
+		p[i] = s2[y];
+		i++;
+	}
+	return (p);
+}
--- 0x0C-more_malloc_free/holberton.h
+++ 0x0C-more_malloc_free/holberton.h
@@ -2,5 +2,6 @@
 #define holberton
 
 void *malloc_checked(unsigned int b);
+char *string_nconcat(char *s1, char *s2, unsigned int n);
 
 #endif
