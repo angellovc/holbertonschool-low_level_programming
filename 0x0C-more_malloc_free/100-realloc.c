0c891f8bead82ccf486b47c11d0878283313a4c2
angellovc <vc.angel@outlook.com>
2020-03-05 23:36:04 +0000

    new file was added

--- 0x0C-more_malloc_free/100-realloc.c
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -11,7 +11,7 @@ void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 	unsigned int i = 0;
 	char *p;
 
-	if (new_size == old_size || new_size < old_size)
+	if (new_size == old_size)
 		return (ptr);
 	else if (new_size == 0 && ptr != '\0')
 	{
