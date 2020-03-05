ef11b66bc3f6e3d15bcc59933f9a8a931a92fdb7
angellovc <vc.angel@outlook.com>
2020-03-05 18:00:49 +0000

    new file was added

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -31,16 +31,25 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 	if (n >= l2)
 	{
 		p = malloc(l1 + l2 + 1);
+		if (p == '\0')
+			return ('\0');
 		n = l2;
 	}
 	else
-		p = malloc(l1 + n + 1);
-	for (i = 0; s1[i] != '\0'; i++)
-		p[i] = s1[i];
-	for (y = 0; y < n; y++)
 	{
-		p[i] = s2[y];
-		i++;
+		p = malloc(l1 + n + 1);
+		if (p == '\0')
+			return ('\0');
 	}
+	if (*s1 != '\0')
+		for (i = 0; s1[i] != '\0'; i++)
+			p[i] = s1[i];
+	if (*s2 != '\0')
+		for (y = 0; y < n; y++)
+		{
+			p[i] = s2[y];
+			i++;
+		}
+	p[i] = '\0';
 	return (p);
 }
