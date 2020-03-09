1b991a0fa486ccc6193d233ce0318be523df8bdb
angellovc <vc.angel@outlook.com>
2020-03-09 17:10:30 +0000

    new file was added

--- 0x0D-preprocessor/main.c
+++ 0x0D-preprocessor/main.c
@@ -1,21 +1,17 @@
+#include "0-object_like_macro.h"
+#include "0-object_like_macro.h"
 #include <stdio.h>
-#include "3-function_like_macro.h"
-#include "3-function_like_macro.h"
 
 /**
  * main - check the code for Holberton School students.
  *
  * Return: Always 0.
  */
-int main(void)
+	int main(void)
 {
-	int i;
-	int j;
+	int s;
 
-	i = ABS(-98);
-	printf("i = %i\n", i);
-	j = ABS(98);
-	printf("j = %i\n", i);
-	printf("%d\n, %d\n", i, j);
+	s = 98 + SIZE;
+	printf("%d\n", s);
 	return (0);
 }
