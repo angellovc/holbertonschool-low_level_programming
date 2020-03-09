e407faf2d143228e14756d9b202c3a5522d4fbef
angellovc <vc.angel@outlook.com>
2020-03-09 21:35:28 +0000

    new file was added

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -12,8 +12,18 @@ struct dog
 	float age;
 	char *owner;
 };
-
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
-
+/**
+ *struct dog_t - this is a dog structure
+ *@name: dog's name
+ *@age: dog's age
+ *@owner: owner's dog
+ */
+typedef struct dog_t
+{
+	char *name;
+	float age;
+	char *owner;
+} dog_t;
 #endif
