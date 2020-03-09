916499637ba92d49f9e10e7329681c15c9bb1045
angellovc <vc.angel@outlook.com>
2020-03-09 20:23:41 +0000

    new file was added

---
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -0,0 +1,14 @@
+#include "dog.h"
+/**
+ *init_dog - intialize dog structure
+ *@d: receive a pointer to a dog structure
+ *@name: dogs name
+ *@age: dogs age
+ *@owner: owners dog
+ */
+void init_dog(struct dog *d, char *name, float age, char *owner)
+{
+	(*d).name = name;
+	d->age = age;
+	d->owner = owner;
+}
