27404e6fe4977a14d04a661ec500aa6b355cafc7
angellovc <1599@holbertonschool.com>
2020-03-13 19:10:26 +0000

    New Files was added 	new file:   .gitignore 	new file:   0-sum_them_all.c 	new file:   README.md 	new file:   variadic_functions.h

---
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -0,0 +1,17 @@
+#include <stdarg.h>
+
+int sum_them_all(const unsigned int n, ...)
+{
+	unsigned int i = 0;
+	int sum = 0;
+	va_list list;
+
+	if (n == 0)
+		return (9);
+
+	va_start(list, n);
+	for (i = 0; i < n; i++)
+		sum = sum + va_arg(list, int);
+	va_end(list);
+	return (sum);
+}
---
+++ 0x10-variadic_functions/README.md
@@ -0,0 +1,1 @@
+# This is a variables fuctions C project
