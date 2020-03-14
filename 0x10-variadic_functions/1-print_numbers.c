ea1703c9efcb4030d8f35a7f87b1a84ef3354fdd
angellovc <1599@holbertonschool.com>
2020-03-14 21:53:18 +0000

    new file added

--- 0x10-variadic_functions/1-print_numbers.c
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -11,12 +11,10 @@ void print_numbers(const char *separator, const unsigned int n, ...)
 	unsigned int i = 0;
 	int number = 0;
 
-	if (n == 0)
-		return;
 	if (separator == '\0' && *separator == '\0')
 		return;
 	va_start(list, n);
-	for (i = 0; i < n; i++)
+	for (i = 0; i < n && n != 0 ; i++)
 	{
 		number = va_arg(list, int);
 		if (i != (n - 1))
