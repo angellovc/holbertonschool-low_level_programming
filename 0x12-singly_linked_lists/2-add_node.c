39e56867ca8c1b76c771264ba55c7d02c0593205
angellovc <vc.angel@outlook.com>
2020-03-19 21:02:22 +0000

    new files was added

--- 0x12-singly_linked_lists/2-add_node.c
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -11,7 +11,7 @@ list_t *add_node(list_t **head, const char *str)
 	list_t *new, *tmp;
 	int len = 0;
 
-	while (str[len] != '\0')
+	while (str != '\0' && str[len] != '\0')
 		len++;
 	tmp = *head;
 	new = malloc(sizeof(list_t));
@@ -20,11 +20,12 @@ list_t *add_node(list_t **head, const char *str)
 	new->str = malloc(sizeof(len));
 	if (new->str == '\0')
 		return ('\0');
-	new->str = strdup(str);
+	if (str != '\0')
+		new->str = strdup(str);
+	else
+		new->str = '\0';
 	new->len = len;
 	*head = new;
 	new->next = tmp;
 	return (new);
-
 }
-
