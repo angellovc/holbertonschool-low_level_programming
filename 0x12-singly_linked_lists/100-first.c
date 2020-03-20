0fe5c70c7d04c553f242f6b1ed26d8e194864ef6
angellovc <vc.angel@outlook.com>
2020-03-20 01:25:18 +0000

    succesfull

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,10 @@
+#include "lists.h"
+#include <stdio.h>
+/**
+ *turtle - printf before main function
+ */
+void turtle(void)
+{
+	printf("You're beat! and yet, you must allow,\n");
+	printf("I bore my house upon my back!\n");
+}
