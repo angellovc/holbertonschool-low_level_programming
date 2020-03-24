bbaa0a109e1db1d1adea9da0f61823a1fda1196e
angellovc <vc.angel@outlook.com>
2020-03-24 17:11:09 +0000

    9 file was modified

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -1,5 +1,23 @@
 #include "lists.h"
 /**
+*new_listint - make a new listint struct
+*@n: element of the new struct
+*Return: pointer to a new struct
+*/
+listint_t *new_listint(int n)
+{
+	listint_t *new;
+
+	new = malloc(sizeof(listint_t));
+	if (new == '\0')
+		return ('\0');
+	new->n = 0;
+	new->next = '\0';
+	new->n = n;
+	return (new);
+}
+
+/**
  *insert_nodeint_at_index - insert node into a specific position
  *@head: linked list
  *@idx: position
@@ -13,6 +31,13 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 
 
 	node = (*head);
+	if (idx == 0)
+	{
+		new = new_listint(n);
+		new->next = node;
+		(*head) = new;
+		return (*head);
+	}
 	while (nth < idx)
 	{
 		if (node == '\0' && nth != idx)
@@ -21,12 +46,7 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 		nth++;
 	}
 	tmp = node->next;
-	new = malloc(sizeof(listint_t));
-	if (new == '\0')
-		return ('\0');
-	new->n = 0;
-	new->next = '\0';
-	new->n = n;
+	new = new_listint(n);
 	new->next = tmp;
 	node->next = new;
 	return (new);
