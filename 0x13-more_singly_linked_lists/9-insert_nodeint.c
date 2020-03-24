03eeba2619067161f5d0899a3b94ba434466092f
angellovc <vc.angel@outlook.com>
2020-03-24 22:47:34 +0000

    NODNO

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -44,7 +44,8 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 	if (idx == 0)
 	{
 		new = new_listint(n);
-		new->next = '\0';
+		new->next = (*head);
+		(*head) = new;
 		return (new);
 	}
 	node = (*head);
