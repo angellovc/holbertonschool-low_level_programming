5ef2fd6b4a6c8a4f55e0a59626bd9177ff0446ce
angellovc <vc.angel@outlook.com>
2020-03-24 18:39:29 +0000

    nasd

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -31,7 +31,7 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 
 	if (head == '\0')
 		return ('\0');
-	if ((*head) == '\0' && idx != 0)
+	if ((*head) == '\0')
 		return ('\0');
 	node = (*head);
 	if (idx == 0)
