18f2b50ce52022474dd8a6e9cbaaaad4833e0374
angellovc <vc.angel@outlook.com>
2020-03-24 18:01:41 +0000

    9 was modified

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -29,15 +29,16 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 	listint_t *new, *tmp, *node;
 	unsigned int nth = 0;
 
-	if (head == '\0' || (*head) == '\0')
+	if (head == '\0')
+		return ('\0');
+	if ((*head) == '\0' && idx != 0)
 		return ('\0');
 	node = (*head);
 	if (idx == 0)
 	{
 		new = new_listint(n);
 		new->next = node;
-		(*head) = new;
-		return (*head);
+		return (new);
 	}
 	while (nth < idx)
 	{
