225a49493226df816d8f1f6a8474ae9102f1e758
angellovc <vc.angel@outlook.com>
2020-03-25 19:50:53 +0000

    dadw

--- 0x13-more_singly_linked_lists/101-print_listint_safe.c
+++ 0x13-more_singly_linked_lists/101-print_listint_safe.c
@@ -2,51 +2,28 @@
 #include <stdio.h>
 #include "lists.h"
 /**
- *find_loop - find a loop into a linked list
- *@head: linked list
- *Return: loop position
- */
-listint_t *find_loop(listint_t *head)
-{
-	listint_t *turtle, *bunny;
-
-	bunny = head;
-	turtle = head;
-	while (turtle && bunny && bunny->next)
-	{
-		turtle = turtle->next;
-		bunny = bunny->next->next;
-		if (turtle == bunny)
-			return (bunny->next);
-	}
-	return ('\0');
-}
-/**
  *print_listint_safe - print int avoiding a loop
  *@head: linked list
  *Return: size of the linked list
  */
 size_t print_listint_safe(const listint_t *head)
 {
-	listint_t *node, *tmp;
-	size_t size = 0, step = 1, limit = 2;
+	listint_t *node;
+	size_t size = 0;
 
 	if (head == '\0')
 		return ('\0');
-
 	node = (listint_t *)head;
-	tmp = find_loop(node);
 	while (node != '\0')
 	{
-		if (step != limit)
 			printf("[%p] %i\n", (void *)node, node->n);
-		if (step == limit)
+		if (node - node->next <= 0)
 		{
+			node = node->next;
+			size++;
 			printf("-> [%p] %i\n", (void *)node, node->n);
 			break;
 		}
-		if (node == tmp)
-			step++;
 		node = node->next;
 		size++;
 	}
