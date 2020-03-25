ec441a278609272ab1f0212aac1aed8c9209022a
angellovc <vc.angel@outlook.com>
2020-03-25 02:37:37 +0000

    daodmao

--- 0x13-more_singly_linked_lists/101-print_listint_safe.c
+++ 0x13-more_singly_linked_lists/101-print_listint_safe.c
@@ -8,31 +8,32 @@
  */
 size_t print_listint_safe(const listint_t *head)
 {
-	const listint_t *turtle, *bunny, *tmp;
+	listint_t *turtle, *bunny, *tmp;
 	size_t size = 0;
 
 	if (head == '\0')
 		exit(98);
-	bunny = head;
-	turtle = head;
+	bunny = (listint_t *)head;
+	turtle = (listint_t *)head;
+	tmp = '\0';
 	while (head != '\0')
 	{
-		size++;
 		turtle = turtle->next;
 		if (bunny != '\0' && bunny->next->next != '\0')
 			bunny = bunny->next->next;
 		else
 			bunny = '\0';
-		if (!(tmp == turtle || tmp == bunny))
+		if (!((tmp != '\0') && (tmp == turtle || tmp == bunny)))
 			printf("[%p] %i\n", (void *)head, head->n);
-		if (tmp == turtle || tmp == bunny)
+		if ((tmp != '\0') && (tmp == turtle || tmp == bunny))
 		{
 			printf("-> [%p] %i\n", (void *)head, head->n);
 			break;
 		}
-		if (turtle == bunny)
-			tmp = bunny;
+		if (turtle == bunny && size != 0)
+			tmp = turtle;
 		head = head->next;
+		size++;
 	}
 	return (size);
 }
