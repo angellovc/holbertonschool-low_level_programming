35ad797e9fa9ce732aad8a633ae7e991d72e6549
angellovc <vc.angel@outlook.com>
2020-03-26 22:27:39 +0000

    dada

---
+++ 0x14-bit_manipulation/3-set_bit.c
@@ -0,0 +1,18 @@
+#include "holberton.h"
+/**
+ *set_bit - set an specific bit position into 1
+ *@index: bit position
+ *@n: number
+ *Return: number modified
+ */
+int set_bit(unsigned long int *n, unsigned int index)
+{
+	unsigned long int i = 1;
+
+	if (index >= sizeof(unsigned long int) * 8)
+		return (-1);
+
+	i = i << index;
+	*n = *n | i;
+	return (*n);
+}
