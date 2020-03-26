4b46eaf10a1521eb1119863863353befd394a8e9
angellovc <vc.angel@outlook.com>
2020-03-26 21:25:21 +0000

    new files was added2

---
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -0,0 +1,19 @@
+#include "holberton.h"
+#include <stdio.h>
+/**
+ *get_bit - get the value of a specific bit into a binary number
+ *@n: number
+ *@index: specific bit position
+ *Return: bit value
+ */
+int get_bit(unsigned long int n, unsigned int index)
+{
+	unsigned long int tmp = n, i;
+
+	for (i = 0; tmp != 0; i++)
+		tmp = tmp >> 1;
+	if (i <= index)
+		return (-1);
+	n = n >> index;
+	return (n & 1);
+}
