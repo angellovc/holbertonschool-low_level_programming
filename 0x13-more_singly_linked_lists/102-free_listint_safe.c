348459f8b4c4ed25669ab0817defd457639ca20e
angellovc <vc.angel@outlook.com>
2020-03-27 18:24:25 +0000

    new file added

---
+++ 0x13-more_singly_linked_lists/102-free_listint_safe.c
@@ -0,0 +1,28 @@
+#include "lists.h"
+/**
+ *free_listint_safe - free in a safe way
+ *@h: linked list
+ *Return: size of released nodes
+ */
+size_t free_listint_safe(listint_t **h)
+{
+	listint_t *tmp;
+	size_t size = 0;
+
+	if (h == '\0' || (*h) == '\0')
+		return (0);
+	while ((*h) != '\0')
+	{
+		if ((*h) - (*h)->next <= 0)
+		{
+			free(tmp);
+			size++;
+			break;
+		}
+		tmp = (*h)->next;
+		free(*h);
+		(*h) = tmp;
+		size++;
+	}
+	return (size);
+}
