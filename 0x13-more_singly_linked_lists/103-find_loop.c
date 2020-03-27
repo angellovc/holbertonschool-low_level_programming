f95cc9bd4acecd281d2919f6bce844b9c8cb49e8
angellovc <vc.angel@outlook.com>
2020-03-27 21:24:35 +0000

    nice

--- 0x13-more_singly_linked_lists/103-find_loop.c
+++ 0x13-more_singly_linked_lists/103-find_loop.c
@@ -7,12 +7,13 @@
 listint_t *find_listint_loop(listint_t *head)
 {
 	listint_t *node;
+
 	if (head == '\0')
 		return ('\0');
 	node = head;
 	while (node != '\0')
 	{
-		if (node- node->next <= 0)
+		if (node - node->next <= 0)
 			return (node->next);
 		node = node->next;
 	}
