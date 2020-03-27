cadd0cb073a06e8cbb08d41417e5f8d3a4e5ae4a
angellovc <vc.angel@outlook.com>
2020-03-27 19:25:23 +0000

    new

--- 0x13-more_singly_linked_lists/103-find_loop.c
+++ 0x13-more_singly_linked_lists/103-find_loop.c
@@ -6,16 +6,27 @@
  */
 listint_t *find_listint_loop(listint_t *head)
 {
-	listint_t *node;
+	listint_t *bunny, *turtle;
 
 	if (head == '\0')
 		return ('\0');
-	node = head;
-	while (node != '\0')
+	bunny = head;
+	turtle = head;
+	while (bunny != '\0' && turtle != '\0')
 	{
-		if (node - node->next <= 0)
-			return (node->next);
-		node = node->next;
+		turtle = turtle->next;
+		bunny = bunny->next->next;
+		if (bunny == turtle)
+			return ('\0');
+	}
+	if (bunny == '\0' || bunny->next == '\0')
+		return ('\0');
+	turtle = head;
+	while (turtle != '\0')
+	{
+		if (turtle - turtle->next <= 0)
+			return (turtle->next);
+		turtle = turtle->next;
 	}
 	return ('\0');
 }
