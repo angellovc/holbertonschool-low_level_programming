0c88cd9e32744364ab32567a0176a53f2dd6a75d
angellovc <vc.angel@outlook.com>
2020-03-31 23:09:49 +0000

    new file added

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -4,21 +4,21 @@
 #include <fcntl.h>
 #include <unistd.h>
 /**
- *create_file -replace or make a new file and put text inside
- *@filename: name of the file
+ *append_text_to_file - add text at the end of an existing file
+ *@filename: file name
  *@text_content: text
  *Return: 1 succes, -1 error
  */
-int create_file(const char *filename, char *text_content)
+int append_text_to_file(const char *filename, char *text_content)
 {
 	int fd, i = 0, y = 0;
 
 	if (filename == '\0')
 		return (-1);
-	fd = open(filename, O_CREAT | O_TRUNC | O_WRONLY, 0600);
+	fd = open(filename, O_RDWR | O_APPEND);
 	if (fd == -1)
 		return (-1);
-	while (text_content != '\0' && text_content[i] != '\0')
+	while (text_content && text_content[i])
 		i++;
 	y = write(fd, text_content, i);
 	if (y == -1)
