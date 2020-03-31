5965ff020fa8df02d63cde54d2204cb9b253181c
angellovc <vc.angel@outlook.com>
2020-03-31 20:22:40 +0000

    dw

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -1,9 +1,14 @@
 #include "holberton.h"
-#include <stdio.h>
 #include<sys/types.h>
 #include <sys/stat.h>
 #include <fcntl.h>
 #include <unistd.h>
+/**
+ *read_textfile - print a number of letters into a file
+ *@filename: name of the file
+ *@letters: number of letters
+ *Return: return the number of letters printed
+ */
 ssize_t read_textfile(const char *filename, size_t letters)
 {
 	int fd;
