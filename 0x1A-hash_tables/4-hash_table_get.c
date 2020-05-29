8739001fb08f1438380112b0bac60d4e627c1e09
angellovc <vc.angel@outlook.com>
2020-05-29 15:13:07 +0000

    4-hash_table_get.c was added

---
+++ 0x1A-hash_tables/4-hash_table_get.c
@@ -0,0 +1,25 @@
+#include "hash_tables.h"
+/**
+ * hash_table_get - get a value of a specific key
+ * @ht: hash table
+ * @key: key
+ * Return: Value in success or Null in failure
+ */
+char *hash_table_get(const hash_table_t *ht, const char *key)
+{
+	unsigned long int index;
+	hash_node_t *node;
+
+	if (ht == '\0' || key == '\0' || strlen(key) == 0)
+		return ('\0');
+	index = key_index((const unsigned char *)key, ht->size);
+	node = ht->array[index];
+	if (node == '\0')
+		return ('\0');
+	while (node != '\0')
+	{
+		if (strcmp(node->key, key) == 0)
+			return (node->value);
+	}
+	return ('\0');
+}
