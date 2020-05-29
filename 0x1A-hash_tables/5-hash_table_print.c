c5acd0d8cb75bc0911f943a429762a786cef7ab3
angellovc <vc.angel@outlook.com>
2020-05-29 16:50:40 +0000

    5-hash_table_print.c was added

---
+++ 0x1A-hash_tables/5-hash_table_print.c
@@ -0,0 +1,41 @@
+#include "hash_tables.h"
+/**
+ *print_hash_node - print nodes of the hasth table
+ *@node: node
+ *
+ */
+void print_hash_node(hash_node_t *node)
+{
+	while (node != '\0')
+	{
+		printf("'%s': ", node->key);
+		printf("'%s'", node->value);
+		node = node->next;
+	}
+
+}
+/**
+ *hash_table_print - print the entire elements into a hash table
+ *@ht: hash table
+ */
+void hash_table_print(const hash_table_t *ht)
+{
+	hash_node_t *node;
+	unsigned long int i = 0;
+	int up = 0;
+
+	printf("{");
+	while (i < ht->size)
+	{
+		if (ht->array[i] != NULL)
+		{
+			if (up == 1)
+				printf(", ");
+			node = ht->array[i];
+			pint_hash_node(ht->array[i]);
+			up = 1;
+		}
+		i++;
+	}
+	printf("}\n");
+}
