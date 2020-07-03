f78d969adee616cc680b6536c07a29cb98d5e981
angellovc <vc.angel@outlook.com>
2020-07-03 18:47:07 +0000

    new project was added

---
+++ 0x1C-makefiles/holbie_func.h
@@ -0,0 +1,9 @@
+#ifndef HOLBIE_FUNC_H
+#define HOLBIE_FUNC_H
+
+#include <stdio.h>
+
+void print_hippocamper(void);
+
+
+#endif
---
+++ 0x1C-makefiles/main.c
@@ -0,0 +1,9 @@
+#include "holbie_func.h"
+/**
+ * main - entry point of print_hippocamper
+ * Return: always 0
+ */
+int main(void)
+{
+	print_hippocamper();
+}
