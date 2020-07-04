c7c1e66eb36b527e01fc10ecb7f3065ca7f8980f
angellovc <vc.angel@outlook.com>
2020-07-04 01:38:43 +0000

    5-island_perimeter.py

---
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -0,0 +1,46 @@
+#!/usr/bin/python3
+""" island_perimeter module """
+
+
+def island_perimeter(grid):
+    """ runs through the grid
+    checking the perimeter of
+    land numbers
+    1 represent the land
+    0 represent the whater around
+    Args:
+        grid ([double list]):
+    Returns:
+        [perimeter]: [land perimeter]
+    """
+    perimeter = 0
+    counter = 0
+    for row, y in zip(grid, range(0, len(grid))):
+        for x in range(0, len(row)):
+            if row[x] == 1:
+                perimeter = perimeter + check_perimeter(grid, x, y)
+    return perimeter
+
+
+def check_perimeter(grid, x, y):
+    """check the perimeter of a
+    given position
+    1 represent land
+    0 represent wather
+    Args:
+        grid ([list]): [list of lists]
+        x ([x axis]): [x axis of the list]
+        y ([y axis]): [y axis of the list]
+    Returns:
+        [counter]: [the total sum of each 0 around the given position]
+    """
+    counter = 0
+    if grid[y][x + 1] == 0:
+        counter += 1
+    if grid[y][x - 1] == 0:
+        counter += 1
+    if grid[y - 1][x] == 0:
+        counter += 1
+    if grid[y + 1][x] == 0:
+        counter += 1
+    return counter
