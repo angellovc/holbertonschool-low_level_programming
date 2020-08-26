1e0c501ff14480aee9bb6812d364810cb0f87c49
angellovc <vc.angel@outlook.com>
2020-08-26 02:53:43 +0000

    new project

---
+++ 0x1E-search_algorithms/0-linear.c
@@ -0,0 +1,24 @@
+#include "search_algos.h"
+/**
+ *linear_search -  function that searches for a value in
+ *an array of integers using the Linear search algorithm
+ * @array: elements collections
+ * @size: array size
+ * @value: value to search for
+ * Return: array index of the coincidence or -1 in failure
+ **/
+int linear_search(int *array, size_t size, int value)
+{
+	int i = 0;
+
+	if (array == NULL)
+		return (-1);
+	while (i < size)
+	{
+		printf("Value checked array[%i] = [%i]\n", i, array[i]);
+		if (array[i] == value)
+			return (i);
+		i++;
+	}
+	return (-1);
+}
---
+++ 0x1E-search_algorithms/1-binary.c
@@ -0,0 +1,60 @@
+#include "search_algos.h"
+/**
+ *print_search - print an array section
+ *in which binary functions are searching for
+ * @array: array
+ * @first: first array element
+ * @last: last array element
+ */
+void print_search(int *array, size_t first, size_t last)
+{
+	int i = first;
+
+	printf("Searching in array: ");
+	for (; i <= last; i++)
+		if (i < last)
+			printf("%i, ", array[i]);
+		else
+			printf("%i", array[i]);
+	printf("\n");
+}
+/**
+ *binary - binary search algorithm
+ *@array: array
+ *@first: first array element
+ *@last: last array element
+ *@value: value to search for
+ *Return: the first occurence index or -1 in failure
+ */
+int binary(int *array, size_t first, size_t last, int value)
+{
+	int i = first;
+	size_t mid = 0;
+
+	mid = (first + last) / 2;
+
+	print_search(array, first, last);
+
+	if (array[mid] == value)
+		return (mid);
+
+	if (first >= last)
+		return (-1);
+
+	if (array[mid] < value)
+		return (binary(array, mid + 1, last, value));
+	return (binary(array, first, mid - 1, value));
+}
+/**
+ *binary_search - binary search algorithm
+ *@array: array
+ *@size: array size
+ *@value: value to search for
+ *Return: the first occurence index or -1 in failure
+ */
+int binary_search(int *array, size_t size, int value)
+{
+	int i = 0;
+
+	return (binary(array, 0, size, value));
+}
