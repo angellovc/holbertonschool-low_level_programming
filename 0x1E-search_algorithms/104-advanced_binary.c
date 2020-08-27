6671576776ab96fdce5c1aa7a27cccc15d2bd904
angellovc <vc.angel@outlook.com>
2020-08-27 23:08:06 +0000

    104-advanced_binary.c

--- 0x1E-search_algorithms/104-advanced_binary.c
+++ 0x1E-search_algorithms/104-advanced_binary.c
@@ -36,7 +36,9 @@ int binary(int *array, size_t first, size_t last, int value)
 
 	if (array[mid] == value && array[mid - 1] != value)
 		return (mid);
-	if (array[mid - 1] == value && array[mid] == value)
+	if (array[mid] == value && mid == 0)
+		return (mid);
+	if (array[mid - 1] == value)
 		mid++;
 	if (first >= last)
 		return (-1);
