454aacb37663d6214f39adcd0cb13ba6464b8c81
angellovc <vc.angel@outlook.com>
2020-08-27 18:08:13 +0000

    100-jump.c

--- 0x1E-search_algorithms/100-jump.c
+++ 0x1E-search_algorithms/100-jump.c
@@ -10,17 +10,18 @@
  **/
 int jump_algorithm(int *array, size_t size, size_t position, int value)
 {
-	size_t jump = (size_t)sqrt(size);
+	size_t jump = (size_t)sqrt(size), last;
 
 	if (array[position] >= value)
 	{
 		if ((int)position - (int)jump >= 0)
 			position = position - jump;
+		last = position + jump;
 		printf("Value found between indexes [%li] and [%li]\n",
 			position,
 			position + jump
 		);
-		while (position <= position + jump)
+		while (position <= last)
 		{
 			printf("Value checked array[%li] = [%i]\n", position, array[position]);
 			if (array[position] == value)
