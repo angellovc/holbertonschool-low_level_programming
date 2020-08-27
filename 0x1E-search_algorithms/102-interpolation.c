11ebc26b20efd26b6705e08331978f00d7e8e8af
angellovc <vc.angel@outlook.com>
2020-08-27 21:49:24 +0000

    102-interpolation

---
+++ 0x1E-search_algorithms/102-interpolation.c
@@ -0,0 +1,46 @@
+#include "search_algos.h"
+/**
+ * interpolation_algorithm - interpolation algorithm implementation
+ * @array: elements collection
+ * @low: array low range
+ * @high: array high range
+ * @value: value to search for
+ * Return: index of the searched element or 0 in failure
+ **/
+int interpolation_algorithm(int *array, size_t low, size_t high, int value)
+{
+	size_t pos;
+
+	pos = ((double)(high - low) / (array[high] - array[low]));
+	pos *= (value - array[low]);
+	pos += low;
+
+	if (low > high || value < array[low] || value > array[high])
+	{
+		printf("Value checked array[%li] is out of range\n", pos);
+		return (-1);
+	}
+	printf("Value checked array[%li] = [%i]\n", pos, array[pos]);
+
+	if (array[pos] == value)
+		return (pos);
+
+	if (array[pos] < value)
+		return (interpolation_algorithm(array, pos + 1, high, value));
+	else if (array[pos] > value)
+		return (interpolation_algorithm(array, low, pos - 1, value));
+	else
+		return (-1);
+}
+/**
+ * interpolation_search - prepare the terrain for
+ * interpolation algorithm implementation
+ * @array: elements collection
+ * @size: array size
+ * @value: value to search for
+ * Return: index of the searched element or 0 in failure
+ **/
+int interpolation_search(int *array, size_t size, int value)
+{
+	return (interpolation_algorithm(array, 0, size - 1, value));
+}
